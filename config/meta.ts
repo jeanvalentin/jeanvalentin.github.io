6af506c7a03a79464ec144a3cd4e4ae022df09bf
Jean Valentin <jeanvalentin@users.noreply.github.com>
2022-11-20 09:29:34 +0100

    Add article node-db2

--- config/meta.ts
+++ config/meta.ts
@@ -15,6 +15,11 @@ export const metas: Meta[] = [
     title: `Release a new version of a JavaScript application to cPanel`,
     date: '2022-11-13',
   },
+  {
+    slug: 'node-db2',
+    title: `Connect to a DB2 instance with Node.js`,
+    date: '2022-11-20',
+  },
 ];
 
 export const getMetaBySlug = (slug: string) => metas.find(v => v.slug === slug);
